ec52b07b82473c8e8ee4d82439db54c19dadcf6b
Pandey <vibhupandey28@gmail.com>
2026-08-11 06:25:41 +0530

    Fix parsing of a parenthesized left operand of a set operator (#312)

--- parser/walk.go
+++ parser/walk.go
@@ -19,6 +19,9 @@ func Walk(node Expr, fn WalkFunc) bool {
 
 	switch n := node.(type) {
 	case *SelectQuery:
+		if !Walk(n.InnerQuery, fn) {
+			return false
+		}
 		if !Walk(n.With, fn) {
 			return false
 		}
